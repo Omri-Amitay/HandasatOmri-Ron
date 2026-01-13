042a9da11fdee6e7b3610bc242988f77dbb4fe61
omriamitay12 <omriamitay12@gmail.com>
2026-01-13 14:55:40 +0200

    finished motor class

--- HandasatProject/Motor.cpp
+++ HandasatProject/Motor.cpp
@@ -1,9 +1,69 @@
 #include "Arduino.h"
 #include "Motor.h"
 
-Motor::Motor(int motorPin, int directionPin, int signalPin, bool reversed){
+Motor::Motor(int motorPin, int directionPin, int signalPin, bool reversed) {
   _motorPin = motorPin;
   _directionPin = directionPin;
   _signalPin = signalPin;
   _reversed = reversed;
+}
+
+void Motor::init() {
+  for (int i = 0; i < _totalReadings; i++) { _smoothingArray[i] = 0; }
+  pinMode(_motorPin, OUTPUT);
+  pinMode(_directionPin, OUTPUT);
+  pinMode(_signalPin, INPUT_PULLUP);
+}
+
+unsigned Motor::getFloatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void Motor::setPower(float power, int direction) {
+  analogWrite(_motorPin, power);
+  digitalWrite(_directionPin, direction);
+}
+
+unsigned long Motor::getRawSignal() {
+  unsigned long highPulse = pulseIn(_signalPin, HIGH, _slowestPulse);
+
+  if (highPulse < _fastestPulse) {
+    highPulse = _slowestPulse;
+  }
+  return highPulse;
+}
+
+unsigned long Motor::getFilteredSignal() {
+
+  _total -= _smoothingArray[readIndex];
+
+  unsigned long highPulse = Motor::getRawSignal();
+
+  _smoothingArray[readIndex] = highPulse;
+
+  /*Serial.print(" AddingValue: " + String(smoothingArray[readIndex]));*/
+
+  _total += _smoothingArray[readIndex];
+  float average = _total / _totalReadings;
+
+  /* Serial.print("Average Sample: " + String(average) + " Current Index: " + String(readIndex) + " Total: " + String(total) + " ");
+  Serial.print(" [");
+  for (int i = 0; i < totalReadings; i++) { Serial.print(String(smoothingArray[i]) + ", "); }
+  Serial.print("]");
+  */
+  readIndex = (readIndex + 1) % _totalReadings;
+
+  if (readIndex == 9) {
+    _valueSmoothed = true;
+  }
+
+  return average;
+}
+
+float Motor::getCalculatedRPM() {
+  return 26883 * pow(Motor::getFilteredSignal(), -1);
+}
+
+void Motor::update() {
+  Motor::getFilteredSignal();  //this causes the signal list to keep updating resulting in new RPM values.
 }
--- HandasatProject/Motor.h
+++ HandasatProject/Motor.h
@@ -7,23 +7,29 @@ class Motor{
   public:
 
     Motor(int motorPin, int directionPin, int signalPin, bool reversed);
-    void begin();
+    void init();
     void setMotorCalibrationMode(bool isEnabled);
 
-    void setPower(int power, int direction);
+    void setPower(float power, int direction);
 
     unsigned long getRawSignal();
     unsigned long getFilteredSignal();
     float getCalculatedRPM();
+    void update();
 
   private:
-
+    unsigned getFloatMap(float x, float in_min, float in_max, float out_min, float out_max);
     int _motorPin;
     int _directionPin;
     int _signalPin;
     bool _reversed;
 
     bool _motorCalibrationState;
+    int readIndex = 0;
+    long _slowestPulse = 18000;
+    long _fastestPulse = 420;
+    unsigned long _total = 0;
+    bool _valueSmoothed = false;
 
     const int _totalReadings = 10;
     long _smoothingArray[10];
